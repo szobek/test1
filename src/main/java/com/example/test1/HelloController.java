050f862df49067d982622b954979b46893575b4c
Kunszt Norbert <kunszt.norbert@gmail.com>
2022-11-02 11:20:51 +0100

    event probléma

--- src/main/java/com/example/test1/HelloController.java
+++ src/main/java/com/example/test1/HelloController.java
@@ -2,14 +2,13 @@ package com.example.test1;
 
 
 import javafx.collections.ObservableList;
-request frszimport javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.MouseEvent;
+
 
 
 public class HelloController {
@@ -20,13 +19,8 @@ public class HelloController {
     @FXML
     public void clickItem(MouseEvent event)
     {
-        if (event.getClickCount() == 2) //Checking double click
-        {
-            System.out.println("++++++++++++++++++++++!!!!!!!!!!!!!!!!!!!!!");
-            System.out.println(frszTable);
-        }
+        System.out.println(event.getSource());
     }
-
 
     public void setFrszTable() {
         TableColumn frszCol = new TableColumn("frsz");
@@ -37,12 +31,13 @@ public class HelloController {
 
 
 
+frszCol.addEventHandler(MouseEvent.MOUSE_CLICKED, e -> {
+    System.out.println("ez választva");
+        }
+) ;
 
         frszCol.setCellValueFactory(new PropertyValueFactory<Cars, String>("frsz"));
         vinCol.setCellValueFactory(new PropertyValueFactory<Cars, String>("vin"));
-
-        ActionEvent actionEvent = new ActionEvent();
-
         frszTable.getColumns().addAll(frszCol,vinCol);
 
         DB r = new DB();
@@ -51,11 +46,7 @@ public class HelloController {
         frszTable.setItems(data);
 
     }
-    public void setRouteTable(){
-        DB r = new DB();
-        r.insertData(new Cars("aaa-111","","","","",1,1769.0,55));
-        setFrszTable();
-    }
 
 
 }
+
